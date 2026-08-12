30268627d5635cf020c7aa9fdec79560412d2c95
sevesilvsuno-prog <sevesilv.suno@gmail.com>
2026-08-12 18:52:22 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
    ============================================================ */
 'use strict';
 
-const CACHE_NOM = 'mentor-coquille-v10'; // incrémenté à chaque livraison : purge l'ancienne version en cache
+const CACHE_NOM = 'mentor-coquille-v11'; // incrémenté à chaque livraison : purge l'ancienne version en cache
 const FICHIERS_COQUILLE = [
   './',
   './index.html',
